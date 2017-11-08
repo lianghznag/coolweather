1d19254774d3fe01f3d135d0aab0748545b62709
lianzhang <745323307@qq.com>
2017-11-08 09:33:55 +0800

    11

--- app/src/main/java/com/example/administrator/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/WeatherActivity.java
@@ -7,7 +7,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
